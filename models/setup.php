56789eb4cce2ae2fff0bf46b472afe4b7cc25fc8
Hugo Leisink <hugo@leisink.net>
2017-09-18 16:06:28 +0200

    Small changes to setup

--- models/setup.php
+++ models/setup.php
@@ -46,10 +46,6 @@
 				return "update_db";
 			}
 
-			if ($this->settings->pia_version < 12) {
-				return "update_pia";
-			}
-
 			$result = $db->execute("select password from users where username=%s", "admin");
 			if ($result[0]["password"] == "none") {
 				return "credentials";
@@ -194,160 +190,10 @@
 			return $version;
 		}
 
-		/* Add setting when missing
-		 */
-		private function ensure_setting($key, $type, $value) {
-			if ($this->db->entry("settings", $key, "key") != false) {
-				return true;
-			}
-
-			$entry = array(
-				"key"   => $key,
-				"type"  => $type,
-				"value" => $value);
-			return $this->db->insert("settings", $entry) !== false;
-		}
-
 		/* Update database
 		 */
 		public function update_database() {
 			system("../database/private_modules");
-
-			if ($this->settings->database_version < 1) {
-				$this->settings->database_version = 1;
-			}
-
-			if ($this->settings->database_version < 2) {
-				$this->ensure_setting("hiawatha_cache_enabled", "boolean", "false");
-				$this->ensure_setting("hiawatha_cache_default_time", "integer", "3600");
-				$this->ensure_setting("session_timeout", "integer", "3600");
-				$this->ensure_setting("session_persistent", "boolean", "false");
-
-				$this->settings->database_version = 2;
-			}
-
-			if ($this->settings->database_version < 3) {
-				$this->db->query("alter table %S add %S varchar(100) not null after %S", "pia_rules", "law_section", "no_next");
-				$this->db->query("alter table %S change %S %S tinyint unsigned not null", "bia", "integrity", "integrity");
-				$this->db->query("alter table %S change %S %S tinyint unsigned not null", "bia", "confidentiality", "confidentiality");
-				$this->db->query("alter table %S drop %S", "bia", "rpo");
-				$this->db->query("alter table %S drop %S", "bia", "rto");
-				$this->db->query("alter table %S add %S tinyint unsigned not null after %S", "bia", "availability", "impact");
-				$this->db->query("alter table %S drop %S", "iso_measures", "description");
-
-				$this->db->query("create table %S (%S int(10) unsigned not null, %S int(10) unsigned not null, ".
-				                 "%S int(10) unsigned not null, key %S (%S), key %S (%S), key %S (%S), ".
-				                 "constraint %S foreign key (%S) references %S (%S), ".
-				                 "constraint %S foreign key (%S) references %S (%S), ".
-				                 "constraint %S foreign key (%S) references %S (%S)".
-				                 ") engine=innodb default charset=utf8",
-					"case_bia_threat", "case_id", "bia_id", "threat_id",
-					"bia_id", "bia_id", "threat_id", "threat_id", "case_id", "case_id",
-					"case_bia_threat_ibfk_3", "threat_id", "threats", "id",
-					"case_bia_threat_ibfk_1", "case_id", "cases", "id",
-					"case_bia_threat_ibfk_2", "bia_id", "bia", "id");
-
-				$this->settings->pia_version = 12;
-
-				$this->settings->database_version = 3;
-			}
-
-			if ($this->settings->database_version < 4) {
-				$this->db->query("alter table %S change %S %S varchar(256) character set utf8 ".
-				                 "collate utf8_general_ci not null",
-					"sessions", "session_id", "session_id");
-
-				$this->db->query("alter table %S add %S text not null after %S", "cases", "impact", "description");
-				$this->db->query("alter table %S add %S varchar(250) null after %S", "cases", "logo", "impact");
-
-				$this->db->query("create table %S (%S int(10) unsigned NOT NULL auto_increment, ".
-				                 "%S int(10) unsigned not null, %S tinyint(4) not null, ".
-				                 "%S varchar(100) not null, primary key (%S), ".
-				                 "key %S (%S), constraint %S foreign key (%S) references %S (%S) ".
-				                 ") engine=InnoDB default charset=utf8", 
-					"iso_measure_categories", "id", "iso_standard_id", "number", "name",
-					"id", "iso_standard", "iso_standard_id",
-					"iso_measure_categories_ibfk_1", "iso_standard_id", "iso_standards", "id");
-
-				$this->db->query("create table %S (%S int(10) unsigned not null auto_increment, ".
-				                 "%S int(10) unsigned not null, %S varchar(100) not null,".
-				                 "%S varchar(100) not null, primary key (%S), key %S (%S), ".
-				                 "constraint %S foreign key (%S) references %S (%S) ".
-				                 ") engine=innodb default charset=utf8",
-					"progress_people", "id", "case_id", "name", "email", "id", "case_id", "case_id",
-					"progress_people_ibfk_1", "case_id", "cases", "id");
-
-				$this->db->query("create table %S (%S int(10) unsigned not null, ".
-				                 "%S int(10) unsigned default null, %S int(10) unsigned default null, ".
-				                 "%S int(10) unsigned not null, %S date default null, ".
-				                 "%S text not null, %S tinyint(1) not null, key %S (%S), ".
-				                 "key %S (%S), key %S (%S), key %S (%S), ".
-				                 "constraint %S foreign key (%S) references %S (%S), ".
-				                 "constraint %S foreign key (%S) references %S (%S), ".
-				                 "constraint %S foreign key (%S) references %S (%S), ".
-				                 "constraint %S foreign key (%S) references %S (%S) ".
-				                 ") engine=innodb default charset=utf8;",
-					"progress_tasks", "case_id", "actor_id", "reviewer_id", "iso_measure_id", "deadline",
-					"info", "done", "case_id", "case_id", "progress_people_id", "actor_id", "iso_measure_id",
-					"iso_measure_id", "reviewer_id", "reviewer_id", "progress_tasks_ibfk_1", "case_id",
-					"cases", "id", "progress_tasks_ibfk_3", "iso_measure_id", "iso_measures", "id",
-					"progress_tasks_ibfk_4", "actor_id", "progress_people", "id", "progress_tasks_ibfk_5",
-					"reviewer_id", "progress_people", "id");
-
-				system("mysql -h '".DB_HOSTNAME."' -u '".DB_USERNAME."' --password='".DB_PASSWORD."' '".DB_DATABASE."' < ../database/imc.sql", $result);
-
-				$this->db->query("update roles set %S=%d, %S=%d, %S=%d where id=%d", "voortgang", YES, "voortgang/personen", YES, "voortgang/rapport", YES, USER_ROLE_ID);
-
-				$this->settings->database_version = 4;
-			}
-
-			if ($this->settings->database_version < 5) {
-				$this->db->query("alter table %S add %S varchar(100) not null after %S",
-					"cases", "organisation", "name");
-				$this->db->query("alter table %S change %S %S text character set utf8 ".
-				                 "collate utf8_general_ci not null",
-					"cases", "description", "scope");
-				$this->db->query("alter table %S add %S boolean not null after %S",
-					"cases", "visible", "logo");
-				$this->db->query("alter table %S change %S %S enum(%s, %s, %s) ".
-				                 "character set utf8 collate utf8_general_ci not null",
-					"bia", "location", "location", "intern", "extern", "saas");
-				$this->db->query("alter table %S add %S text null after %S",
-					"pia", "comment", "answer");
-
-				$this->settings->database_version = 5;
-			}
-
-			if ($this->settings->database_version < 5) {
-				$this->db->query("alter table %S add %S smallint unsigned not null after %S, ".
-				                 "add %S smallint unsigned not null after %S",
-					"progress_tasks", "hours_planned", "done", "hours_invested", "hours_planned");
-
-				$this->settings->database_version = 6;
-			}
-
-			return true;
-		}
-
-		public function update_pia() {
-			if ($this->settings->pia_version < 12) {
-				if (($result = $this->db->execute("select count(*) as count from pia")) === false) {
-					return false;
-				}
-
-				if ($result[0]["count"] > 0) {
-					$this->output->add_message("De database bevat nog PIA's. Deze moeten verwijderd worden voordat de PIA regels kunnen worden bijgewerkt.");
-					return false;
-				}
-
-				system("mysql -h '".DB_HOSTNAME."' -u '".DB_USERNAME."' --password='".DB_PASSWORD."' '".DB_DATABASE."' < ../database/pia.sql", $result);
-				if ($result != 0) {
-					$this->output->add_message("Fout bij het bijwerken van de PIA regels.");
-					return false;
-				}
-
-				$this->settings->pia_version = 12;
-			}
 
 			return true;
 		}
